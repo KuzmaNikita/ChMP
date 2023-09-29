ad2c605f8056f7bc0579973804616219270f5ab5
KuzmaNikita <113753628+KuzmaNikita@users.noreply.github.com>
2023-09-29 11:50:44 +0300

    Update MethodNewton.py

--- MethodNewton.py
+++ MethodNewton.py
@@ -1,19 +1,21 @@
 def f(x):
     return x**4 + 2*x**3 + 2*x**2 + 6*x - 5
 
-def f_prime(x):
+def df(x):
     return 4*x**3 + 6*x**2 + 4*x + 6
 
-def newton_method(initial_guess, tolerance):
-    x0 = initial_guess
-    while True:
-        x1 = x0 - f(x0) / f_prime(x0)
-        if abs(x1 - x0) < tolerance:
-            return x1
-        x0 = x1
+def newton_method(f, df, x0, epsilon):
+    x = x0
+    while abs(f(x)) > epsilon:
+        x = x - f(x) / df(x)
+    return x
 
-initial_guess = 1.0  # Початкове наближення
-tolerance = 0.0001   # Точність
+x0_1 = -2.5
+x0_2 = 0.5
+epsilon = 0.0001
 
-result = newton_method(initial_guess, tolerance)
-print("Розв'язок:", result)
+root1 = newton_method(f, df, x0_1, epsilon)
+root2 = newton_method(f, df, x0_2, epsilon)
+
+print(f"Корінь на відрізку [-3, -2]: {root1}")
+print(f"Корінь на відрізку [0, 1]: {root2}")
